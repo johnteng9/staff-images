049251b9242f9c9aead6b962b6514122a67933b5
Vanshaj Singhania <svanshaj2001@gmail.com>
2021-06-13 10:19:24 -0700

    Add SU21 staff images

--- resize_to_fit.py
+++ resize_to_fit.py
@@ -8,9 +8,9 @@ for image in os.listdir(semester):
     path = os.path.join(semester, image)
     im = Image.open(path)
     print(path, im.size)
-    if im.size[0] <= 150:
+    if max(im.size) <= 150:
         continue
-    wratio = 150 / im.size[0]
+    wratio = 150 / max(im.size)
     im = im.convert('RGB')
     im = im.resize((int(wratio * im.size[0]), int(wratio * im.size[1])))
     im.save(path)
